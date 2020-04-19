9b5a899d3f0bf801384a75a410e4fdb4a999b873
$_TYLER <tlb1986@sbcglobal.net>
2020-04-19 00:44:22 -0500

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,41 +2,43 @@
 
 
 HOMEDIRS=("section10_apps3"
-	"section11_pwfeedback"
-	"section12_limited_shell"
-	"section13_path2"
-	"section14_2019-14287"
-	"section1_opensudo"
-	"section2_apps"
-	"section3_apps2"
-	"section4_pagers"
-	"section5_cron"
-	"section6_ldpreload"
-	"section7_installers"
-	"section8_path"
-	"section9_editors")
+        "section11_pwfeedback"
+        "section12_limited_shell"
+        "section13_path2"
+        "section14_2019-14287"
+        "section1_opensudo"
+        "section2_apps"
+        "section3_apps2"
+        "section4_pagers"
+        "section5_cron"
+        "section6_ldpreload"
+        "section7_installers"
+        "section8_path"
+        "section9_editors")
 
 
-cp -v '/etc/passwd.bak' '/etc/passwd' &&\
-rm -v '/etc/passwd.bak' &&\
+cp -v '/etc/passwd.bak' '/etc/passwd' ;\
+rm -v '/etc/passwd.bak' ;\
 
-cp -v '/etc/shadow.bak' '/etc/shadow' &&\
-rm -v '/etc/shadow.bak' &&\
+cp -v '/etc/shadow.bak' '/etc/shadow' ;\
+rm -v '/etc/shadow.bak' ;\
 
-cp -v '/etc/sudoers.bak' '/etc/sudoers' &&\
-rm -v '/etc/sudoers.bak' &&\
+cp -v '/etc/sudoers.bak' '/etc/sudoers' ;\
+rm -v '/etc/sudoers.bak' ;\
 
-cp -v '/etc/crontab.bak' '/etc/crontab' &&\
-rm -v '/etc/crontab.bak' &&\
+cp -v '/etc/crontab.bak' '/etc/crontab' ;\
+rm -v '/etc/crontab.bak' ;\
 
-cp -v '/etc/group.bak' '/etc/group' &&\
-rm -v '/etc/group.bak' &&\
+cp -v '/etc/group.bak' '/etc/group' ;\
+rm -v '/etc/group.bak' ;\
+
+rm -rfv '/etc/sudoers_fifty/'
 
 for dir in ${HOMEDIRS[*]}
 do
-	rm -rfv "/home/${dir}"
+        rm -rfv "/home/${dir}/"
 done
 
 rm -rfv '/home.bak'
 
-echo "[+] ALL DONE!"
+echo "[+] ALL DONE!  Please double-check for any leftover files or entries"
